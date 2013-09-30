855d20dedd3e84ebb3878bf5f13f9e647e175b9e
Arthur Vuillard <arthur.vuillard@akretion.com>
2013-09-30 18:28:45 +0200

    Don't use the dryrun for importing with connector, use a context key

--- connector_base_import/importer.py
+++ connector_base_import/importer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-#   Module for OpenERP 
+#   Module for OpenERP
 #   Copyright (C) 2013 Akretion (http://www.akretion.com).
 #   @author Sébastien BEAU <sebastien.beau@akretion.com>
 #
@@ -26,14 +26,24 @@ from openerp.addons.connector.queue.job import job
 from openerp.addons.connector.session import ConnectorSession
 
 
-class ir_import(orm.TransientModel):
+CONNECTOR_IMPORT_KEY = 'connector_import'
+
+
+class connector_base_import_installed(orm.AbstractModel):
+    _name = 'connector_base_import.connector'
+
+
+class connector_base_import(orm.TransientModel):
     _inherit = 'base_import.import'
- 
+
     def do(self, cr, uid, id, fields, options, dryrun=False, context=None):
         if context is None:
             context = {}
-        context['dryrun'] = dryrun
-        return super(ir_import, self).do(cr, uid, id, fields, options, dryrun=dryrun, context=context)
+        context[CONNECTOR_IMPORT_KEY] = not dryrun
+        return super(connector_base_import, self).do(cr, uid, id, fields,
+                                                     options, dryrun=dryrun,
+                                                     context=context)
+
 
 original_load = orm.BaseModel.load
 
@@ -41,16 +51,22 @@ original_load = orm.BaseModel.load
 @job
 def import_one_line(session, model_name, fields, line):
     model = session.pool[model_name]
-    return original_load(model, session.cr, session.uid, fields, [line], context=session.context)
+    return original_load(model, session.cr, session.uid, fields, [line],
+                         context=session.context)
 
 
 def load(self, cr, uid, fields, data, context=None):
-    #TODO FIXME test if module is installed
-    if not context.get('dryrun'):
+    if context is None:
+        context = {}
+
+    module_installed = bool(self.pool[connector_base_import_installed._name])
+    connector_import = context.get(CONNECTOR_IMPORT_KEY, False)
+    if module_installed and connector_import:
         for line in data:
             session = ConnectorSession(cr, uid, context)
             import_one_line.delay(session, self._name, fields, line)
         return {'messages': []}
     return original_load(self, cr, uid, fields, data, context=context)
+
 
 orm.BaseModel.load = load
